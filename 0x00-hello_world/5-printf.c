3759b9ff12ed45a79ba4014a8bb84752121833c8
EDWIN-MUNENE <eddynesh@gmail.com>
2022-06-30 14:52:32 -0700

    done

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 
 /**
  * main is entry
- * Return always to be zero
+ * Return always to be 0
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-			return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
