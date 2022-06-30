b2cb41a255c4fa8968252b2ce3ae9046373dc115
EDWIN-MUNENE <eddynesh@gmail.com>
2022-06-30 14:44:07 -0700

    done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 
 #include <stdio.h>
 
+/**                                                                             * Return always to be zero                                                      */   
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
